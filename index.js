6f1b6028cd809fbf6d282143ce1cae85744cdfbb
AndraRian <86605562+AndraRian@users.noreply.github.com>
2022-04-26 15:28:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ const express   = require('express');
 const app       = express();
 const port      = 5000;
 
-app.use(express.static('assets'))
-app.use('/css', express.static(__dirname + 'assets/css'))
-app.use('/img', express.static(__dirname + 'assets/img'))
-app.use('/js', express.static(__dirname + 'assets/js'))
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/view/index.html')
